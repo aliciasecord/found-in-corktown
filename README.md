120a9c3fa391bb91c1455d222da26160346f31c4
Alicia Secord <alicia.m.secord@gmail.com>
2019-02-15 11:57:46 -0500

    FirstCommit
    Project uses Bootstrap, FlipclockJS, Mailchip, and Mapbox.
    
    Photos and illustrations by Alicia Secord.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # found-in-corktown
 The digital home base for the Found In Corktown scavenger hunt.
+
+Find the project at https://aliciasecord.github.io/found-in-corktown
---
+++ js/mapbox.js
@@ -0,0 +1,9 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWxpY2lhc2Vjb3JkIiwiYSI6ImNqZ2Q0cW5oeDNybjgyd241cTR1eGMyenYifQ.IyZgMzeCKNb6tyglxHWU8w';
+
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/light-v9',
+  center: [-83.073, 42.3332],
+  zoom: 14,
+  interactive: false
+  });
